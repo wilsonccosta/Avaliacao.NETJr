9661a70f627c4395205862c0f8b558f4af4cf261
wilsonccosta <wilsonc-costa@hotmail.com>
2017-10-01 21:09:48 -0300

    Correções
    Consultando a partir da pasta local do programa.
    Formatando código.

---
+++ WindowsFormsApplication3/WindowsFormsApplication3/Form1.Designer.cs
@@ -0,0 +1,242 @@
+﻿namespace WindowsFormsApplication3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.dateTimePicker2 = new System.Windows.Forms.DateTimePicker();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(101, 328);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Buscar";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(299, 328);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Fechar";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(15, 183);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(237, 20);
+            this.dateTimePicker1.TabIndex = 2;
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(15, 63);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(15, 14);
+            this.checkBox1.TabIndex = 4;
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(384, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Insira a seguir os dados para consultarmos o carro com melhor preço para você:";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(6, 47);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(209, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Você possui o Cartão Fidelidade (Premiun):";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(6, 89);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(123, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Quantidade de Pessoas:";
+            // 
+            // dateTimePicker2
+            // 
+            this.dateTimePicker2.Location = new System.Drawing.Point(15, 222);
+            this.dateTimePicker2.Name = "dateTimePicker2";
+            this.dateTimePicker2.Size = new System.Drawing.Size(237, 20);
+            this.dateTimePicker2.TabIndex = 8;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(6, 167);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(86, 13);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Data da retirada:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(6, 206);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(101, 13);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "Data de devolução:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(6, 144);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(117, 13);
+            this.label6.TabIndex = 11;
+            this.label6.Text = "Dias a serem alocados:";
+            this.label6.Click += new System.EventHandler(this.label6_Click);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(6, 260);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(186, 13);
+            this.label7.TabIndex = 12;
+            this.label7.Text = "Veículo disponível com melhor preço:";
+            this.label7.Click += new System.EventHandler(this.label7_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.AllowDrop = true;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.IntegralHeight = false;
+            this.comboBox1.Items.AddRange(new object[] {
+            "1",
+            "2",
+            "3",
+            "4",
+            "5",
+            "6",
+            "7"});
+            this.comboBox1.Location = new System.Drawing.Point(15, 105);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 13;
+            this.comboBox1.Text = "1";
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(12, 282);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(0, 13);
+            this.label8.TabIndex = 14;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(12, 304);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(0, 13);
+            this.label9.TabIndex = 15;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(474, 373);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.dateTimePicker2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Car Finder";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.DateTimePicker dateTimePicker2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+    }
+}
+
---
+++ WindowsFormsApplication3/WindowsFormsApplication3/Form1.cs
@@ -0,0 +1,391 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace WindowsFormsApplication3
+{
+
+    public partial class Form1 : Form
+    {
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            /* VARIAVEIS*/
+            Boolean CHECK_PREMIUN;
+            DateTime DATA_INICIAL, DATA_FINAL;
+            string arquivoEntrada,arquivoSaida, DadosSouth,DadosNorth,DadosWest,buscador, diaSemaPt, 
+                    mesPt, inputTXT, outputTXT, datasBuscadas, LOCADORA, CARRO_DISPONIVEL, TIPO_CARRO;
+
+            /*Declaração com inicialização*/
+            Boolean ERRO_DATA = false;
+            int QUANTIDADE_DE_PASSAGEIROS = 1;
+            int diasSemana = 0;
+            int diasFimdeSemana = 0;
+            int diasTotal = 0;
+            float valorSouth = 0;
+            float valorWest = 0;
+            float valorNorth = 0;
+            float valorFinal = 0;
+            string[] DATAS;
+            DATAS = new string[100];
+
+
+            /* Inicialização de variaveis*/
+            diaSemaPt = "---";
+            mesPt = "---";
+            LOCADORA = "---";
+            datasBuscadas = "";
+            buscador = "Base de Dados não definida!";
+            CARRO_DISPONIVEL = "";
+
+
+            /* Obtendo diretório da Aplicação para localizar a pasta de dados */
+            string diretorioAtual = Path.GetDirectoryName(System.AppDomain.CurrentDomain.BaseDirectory.ToString());
+            diretorioAtual = diretorioAtual.Remove(diretorioAtual.Length - 9);
+            diretorioAtual = diretorioAtual + "Dados";
+
+
+            /* Setando arquivos de Entrada, Saida e dados */
+            arquivoEntrada = diretorioAtual+@"\Entrada.txt";
+            arquivoSaida = diretorioAtual + @"\Saida.txt";
+            DadosSouth = diretorioAtual + @"\SouthCar.txt";
+            DadosNorth = diretorioAtual + @"\NorthCar.txt";
+            DadosWest = diretorioAtual + @"\WestCar.txt";
+
+
+            /*Função de formatação do dia da semana obtido pelo DatePicker para o formato do input*/
+            void dataFormatador()
+            {
+                if (DATA_INICIAL.DayOfWeek == DayOfWeek.Sunday)
+                {
+                    diaSemaPt = "dom";
+                }
+                if (DATA_INICIAL.DayOfWeek == DayOfWeek.Monday)
+                {
+                    diaSemaPt = "seg";
+                }
+                if (DATA_INICIAL.DayOfWeek == DayOfWeek.Tuesday)
+                {
+                    diaSemaPt = "ter";
+                }
+                if (DATA_INICIAL.DayOfWeek == DayOfWeek.Wednesday)
+                {
+                    diaSemaPt = "qua";
+                }
+                if (DATA_INICIAL.DayOfWeek == DayOfWeek.Thursday)
+                {
+                    diaSemaPt = "qui";
+                }
+                if (DATA_INICIAL.DayOfWeek == DayOfWeek.Friday)
+                {
+                    diaSemaPt = "sex";
+                }
+                if (DATA_INICIAL.DayOfWeek == DayOfWeek.Saturday)
+                {
+                    diaSemaPt = "sab";
+                }
+            }
+
+            /*Função de formatação do mês obtido pelo DatePicker para o formato do input*/
+            void mesFormatador()
+            {
+                if (DATA_INICIAL.Month == 1)
+                {
+                    mesPt = "Jan";
+                }
+                if (DATA_INICIAL.Month == 2)
+                {
+                    mesPt = "Fev";
+                }
+                if (DATA_INICIAL.Month == 3)
+                {
+                    mesPt = "Mar";
+                }
+                if (DATA_INICIAL.Month == 4)
+                {
+                    mesPt = "Abr";
+                }
+                if (DATA_INICIAL.Month == 5)
+                {
+                    mesPt = "Mai";
+                }
+                if (DATA_INICIAL.Month == 6)
+                {
+                    mesPt = "Jun";
+                }
+                if (DATA_INICIAL.Month == 7)
+                {
+                    mesPt = "Jul";
+                }
+                if (DATA_INICIAL.Month == 8)
+                {
+                    mesPt = "Ago";
+                }
+                if (DATA_INICIAL.Month == 9)
+                {
+                    mesPt = "Set";
+                }
+                if (DATA_INICIAL.Month == 10)
+                {
+                    mesPt = "Out";
+                }
+                if (DATA_INICIAL.Month == 11)
+                {
+                    mesPt = "Nov";
+                }
+                if (DATA_INICIAL.Month == 12)
+                {
+                    mesPt = "Dez";
+                }
+            }
+
+            /* Recebendo dados da Interface */
+            DATA_INICIAL = dateTimePicker1.Value;
+            DATA_FINAL = dateTimePicker2.Value;
+            if (comboBox1.Text != "")
+            {
+                QUANTIDADE_DE_PASSAGEIROS = Convert.ToInt16(comboBox1.Text);
+            }
+            CHECK_PREMIUN = checkBox1.Checked;
+            
+            /*Checka se o usuário é premiun ou não e insere o resultado em
+             TIPO_CARRO como Premiun ou Normal*/
+            if (CHECK_PREMIUN == true)
+            {
+                TIPO_CARRO = "Premiun";
+            }
+            else
+            {
+                TIPO_CARRO = "Normal";
+            }
+
+
+            /* Convertendo dias para formato a ser inserido no txt
+             Também faz a contagem de dias da semana e finais de semana*/
+            inputTXT = TIPO_CARRO + ":" + QUANTIDADE_DE_PASSAGEIROS + ":";
+
+            diasTotal = DATA_FINAL.Subtract(DATA_INICIAL).Days;
+
+
+            /*Verificando se a data de retirada está antes da data de devolução do veiuculo */
+            if (diasTotal < 0)
+            {
+                ERRO_DATA = true;
+            }
+
+            for (int i = 0; i <= diasTotal+1 ; i++)
+            {
+
+                /* Transformando dia da semana no formato para consulta*/
+
+                dataFormatador();
+
+                /* Transformando mês no formato para consulta*/
+
+                mesFormatador();
+                
+
+
+                DATAS[i] = Convert.ToString(DATA_INICIAL.Day)+ mesPt + Convert.ToString(DATA_INICIAL.Year)+"("+ diaSemaPt + ")";
+                
+
+                inputTXT = inputTXT + DATAS[i]+ ","; 
+                datasBuscadas = datasBuscadas + DATAS[i] + ",";
+
+
+                //Conta dias da semana alocados e dias de fim de semana alocados.
+                if (DATA_INICIAL.DayOfWeek != DayOfWeek.Sunday &&
+                    DATA_INICIAL.DayOfWeek != DayOfWeek.Saturday)
+                {
+                    diasSemana++;
+                }
+                else
+                {
+                    diasFimdeSemana++;
+                }
+
+                DATA_INICIAL = DATA_INICIAL.AddDays(1);
+            }
+
+
+            /*                             Calculo de custo do aluguel                              */
+            /*                                  tabela de valores                                   */
+            /*      SouthCar    Normal:210/diaSemana;200/diaFDS  Premiun:150/diaSemana;90/diaFDS    */
+            /*      WestCar     Normal:530/diaSemana;200/diaFDS  Premiun:150/diaSemana;90/diaFDS    */
+            /*      NorthCar    Normal:630/diaSemana;600/diaFDS  Premiun:580/diaSemana;590/diaFDS   */
+            
+            if (TIPO_CARRO == "Normal")
+            {
+                valorSouth = (210* diasSemana) + (200* diasFimdeSemana);
+                valorWest = (530* diasSemana) + (200* diasFimdeSemana);
+                valorNorth = (630* diasSemana) + (600* diasFimdeSemana);
+            }
+            else
+            {
+                valorSouth = (150 * diasSemana) + (90 * diasFimdeSemana);
+                valorWest = (150 * diasSemana) + (90 * diasFimdeSemana);
+                valorNorth = (580 * diasSemana) + (590 * diasFimdeSemana);
+            }
+
+
+            if (QUANTIDADE_DE_PASSAGEIROS > 2)
+            {
+                if (QUANTIDADE_DE_PASSAGEIROS > 4)
+                {
+                    /* O usuário possui mais de 4 passageiros, logo precisa de um SUV */
+                    //buscador será em North
+                    valorFinal = valorNorth;
+                    LOCADORA = "NorthCar";
+                    buscador = DadosNorth;
+                }
+                else
+                {
+                    /*Possui entre de 3 a 4 passageiros, pode alugar um carro compacto ou SUV*/
+                    if (valorSouth <= valorNorth)
+                    {
+                        //buscador será em South
+                        valorFinal = valorSouth;
+                        LOCADORA = "SouthCar";
+                        buscador = DadosSouth;
+                    }
+                    else
+                    {
+                        //buscador será em North
+                        valorFinal = valorNorth;
+                        buscador = DadosNorth;
+                        LOCADORA = "NorthCar";
+                    }
+                }
+            }
+            else
+            {
+                /* O usuário possui até passageiros, podendo assim alugar qualquer veiculo */
+                if (valorSouth <= valorWest && valorSouth <= valorNorth)
+                {
+                    //buscador será em South
+                    valorFinal = valorSouth;
+                    buscador = DadosSouth;
+                    LOCADORA = "SouthCar";
+                }
+                if (valorWest <= valorSouth && valorWest <= valorNorth)
+                {
+                    //buscador será em West
+                    valorFinal = valorWest;
+                    buscador = DadosWest;
+                    LOCADORA = "WestCar";
+                }
+                if (valorNorth <= valorSouth && valorNorth <= valorWest)
+                {
+                    //buscador será em North
+                    valorFinal = valorNorth;
+                    buscador = DadosNorth;
+                    LOCADORA = "NorthCar";
+                }
+            }
+
+            /*Preparando strings de input e output, assim como as datas buscadas */
+            if (ERRO_DATA == false) { 
+                inputTXT = inputTXT.Remove(inputTXT.Length -1);
+                datasBuscadas = datasBuscadas.Remove(datasBuscadas.Length - 1);
+                outputTXT = CARRO_DISPONIVEL + ":" + LOCADORA;
+
+
+                /*Sistema de busca na base de dados(txt) com base nos calculos de valor e passageiros obtido*/
+
+                string linhaBusca;
+                StreamReader arquivoDados = new StreamReader(buscador);
+                while ((linhaBusca = arquivoDados.ReadLine()) != null)
+                {
+                    //Console.WriteLine(linhaBusca);
+                    if (linhaBusca.Contains(datasBuscadas))
+                    {
+                        var carro = linhaBusca.Split(':');
+                        CARRO_DISPONIVEL = carro[0];
+                    }
+                }
+                arquivoDados.Close();
+
+
+                /* Escrevendo entrada em um arquivo que registra as entradas feitas*/
+                StreamWriter escreverEntrada = new StreamWriter(arquivoEntrada, true, Encoding.ASCII);
+                escreverEntrada.WriteLine("[" + DateTime.Now + "] " + inputTXT);
+                escreverEntrada.Close();
+
+                /* Escrevendo as saidas obtidas em um arquivo*/
+                StreamWriter escreverSaida = new StreamWriter(arquivoSaida, true, Encoding.ASCII);
+                escreverSaida.WriteLine("[" + DateTime.Now + "] " + outputTXT);
+                escreverSaida.Close();
+
+                /* Retornando Dados ao usuário na Interface */
+                label8.Text = "Melhor preço encontrado R$: " + valorFinal + ",00";
+                label9.Text = "Carro disponível: " + CARRO_DISPONIVEL + " na locadora: " + LOCADORA;
+
+
+            }
+            else
+            {
+                /* Retornando Erro de data*/
+                label8.Text = "ERRO: DATA DE RETIRADA DEPOIS DE DATA DE DEVOLUÇÃO!";
+                label9.Text = "";
+            }
+
+
+
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label6_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void label7_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
